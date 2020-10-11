5ac680af38958deaf4027da0d4b7f2e06ba2ef1b
Jon Chen <bsd@voltaire.sh>
2020-10-11 18:08:49 +0000

    add cache-control header of 1 hour

--- wrapper.sh
+++ wrapper.sh
@@ -7,5 +7,5 @@ aws s3 cp ${BACKUP_TARBALL_URI} /data/world.tar.gz
 cd /worlds
 tar xvf /data/world.tar.gz
 overviewer.py --config /overviewer_cfg.py
-aws s3 sync --storage-class ONEZONE_IA --delete /output/ ${MAP_OUTPUT_URI}
+aws s3 sync --storage-class ONEZONE_IA --cache-control 'max-age=3600' --delete /output/ ${MAP_OUTPUT_URI}
 curl -H 'Content-Type: application/json' -XPOST ${DISCORD_WEBHOOK_URL} -d '{"content": "NEW MAP NEW MAP NEW MAP BRUH CHECK IT OUT https://map.tonkat.su YOU MIGHT NEED TO CLEAR YOUR CACHE"}'
