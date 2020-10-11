7fb0232a48a46a6a5be6376e5ebf41310b4ef4b2
Jon Chen <bsd@voltaire.sh>
2020-10-11 09:18:40 +0000

    fix rendermode for the end

--- overviewer_cfg.py
+++ overviewer_cfg.py
@@ -43,7 +43,7 @@ renders = {
     "the_end": {
         "world": "the_end",
         "title": "The End",
-        "rendermode": "end_smooth_lighting",
+        "rendermode": end_smooth_lighting,
         "defaultzoom": 8,
     }
 }
