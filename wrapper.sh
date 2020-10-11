f71ef1e5593a4f173fa6805cae4a74091562221c
Jon Chen <bsd@voltaire.sh>
2020-10-11 08:16:19 +0000

    set json content-type in webhook

--- wrapper.sh
+++ wrapper.sh
@@ -8,4 +8,4 @@ cd /worlds
 tar xvf /data/world.tar.gz
 overviewer.py --config /overviewer_cfg.py
 aws s3 sync --storage-class ONEZONE_IA --delete /output/ ${MAP_OUTPUT_URI}
-curl -XPOST ${DISCORD_WEBHOOK_URL} -d '{"content": "NEW MAP NEW MAP NEW MAP BRUH CHECK IT OUT https://map.tonkat.su YOU MIGHT NEED TO CLEAR YOUR CACHE"}'
+curl -H 'Content-Type: application/json' -XPOST ${DISCORD_WEBHOOK_URL} -d '{"content": "NEW MAP NEW MAP NEW MAP BRUH CHECK IT OUT https://map.tonkat.su YOU MIGHT NEED TO CLEAR YOUR CACHE"}'
