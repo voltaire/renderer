9d6831f6f16a8ba1e179668c862c4d1c836892c4
Jon Chen <bsd@voltaire.sh>
2020-10-14 12:03:32 +0000

    add support for uploading to digitalocean spaces

--- wrapper.sh
+++ wrapper.sh
@@ -7,5 +7,8 @@ aws s3 cp --no-progress ${BACKUP_TARBALL_URI} /data/world.tar.gz
 cd /worlds
 tar xf /data/world.tar.gz
 overviewer.py --config /overviewer_cfg.py
-aws s3 sync --cache-control 'max-age=3600' --delete /output/ ${MAP_OUTPUT_URI}
+
+AWS_ACCESS_KEY_ID=${DESTINATION_ACCESS_KEY_ID}
+AWS_SECRET_ACCESS_KEY=${DESTINATION_SECRET_ACCESS_KEY}
+aws s3 sync --endpoint=${DESTINATION_BUCKET_ENDPOINT} --delete /output/ ${DESTINATION_BUCKET_URI}
 curl -H 'Content-Type: application/json' -XPOST ${DISCORD_WEBHOOK_URL} -d '{"content": "NEW MAP NEW MAP NEW MAP BRUH CHECK IT OUT https://map.tonkat.su YOU MIGHT NEED TO CLEAR YOUR CACHE"}'
