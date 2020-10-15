ad9b0bbd531e3c35600bb8a868d765b6c6c4329b
Jon Chen <bsd@voltaire.sh>
2020-10-15 01:48:59 +0000

    optional destination access keypair and endopint

--- wrapper.sh
+++ wrapper.sh
@@ -8,7 +8,14 @@ cd /worlds
 tar xf /data/world.tar.gz
 overviewer.py --config /overviewer_cfg.py
 
-AWS_ACCESS_KEY_ID=${DESTINATION_ACCESS_KEY_ID}
-AWS_SECRET_ACCESS_KEY=${DESTINATION_SECRET_ACCESS_KEY}
-aws s3 sync --endpoint=${DESTINATION_BUCKET_ENDPOINT} --acl 'public-read' --delete /output/ ${DESTINATION_BUCKET_URI}
+if [ -n "$DESTINATION_BUCKET_ENDPOINT" ]; then
+    ENDPOINT_FLAG="--endpoint='${DESTINATION_BUCKET_ENDPOINT}'"
+fi
+
+if [ -n "$DESTINATION_ACCESS_KEY_ID" && -n "$DESTINATION_SECRET_ACCESS_KEY"]; then
+    AWS_ACCESS_KEY_ID=${DESTINATION_ACCESS_KEY_ID}
+    AWS_SECRET_ACCESS_KEY=${DESTINATION_SECRET_ACCESS_KEY}
+fi
+
+aws s3 sync "${ENDPOINT_FLAG}" --acl 'public-read' --delete /output/ ${DESTINATION_BUCKET_URI}
 curl -H 'Content-Type: application/json' -XPOST ${DISCORD_WEBHOOK_URL} -d '{"content": "NEW MAP NEW MAP NEW MAP BRUH CHECK IT OUT https://map.tonkat.su YOU MIGHT NEED TO CLEAR YOUR CACHE"}'
